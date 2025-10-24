da51d78bb7e5845fbe34fabe2fa62ff0acb8a6ea
jin <jf.lily.shan@gmail.com>
2025-10-24 09:52:37 +0800

    Initial Deploy version

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from "react-router-dom";
+import useAuth from "../hooks/use-auth.js";
+
+function NavBar() {
+  const {auth, setAuth} = useAuth();
+
+  const handleLogout = () => {
+      window.localStorage.removeItem("token");
+      setAuth({ token: null });
+  };
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        {auth.token ? (
+                  <Link to="/" onClick={handleLogout}>
+                      Log Out
+                  </Link>
+                  ) : (
+                  <Link to="/login">Login</Link>
+   )}
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default NavBar;
